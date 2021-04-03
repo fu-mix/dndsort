2fd23676d0702c1d8ca9acb2c41e3380e87d4849
fumi <fuminaka@gmail.com>
2021-04-04 01:24:01 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,160 +1,29 @@
 import React from 'react';
-import axios from 'axios';
-import MovieList from './movie_list/component/movie_list';
-import Header from './header/component/Header';
-import Search from './search/component/Search';
-import { CssBaseline, Container, Typography, Box } from '@material-ui/core';
-
-const { useEffect, useReducer } = React;
-
-export interface Movie {
-  Title: string;
-  Year: string;
-  imdbID: string;
-  Type: string;
-  Poster: string;
-  Rated: string;
-  Released: string;
-  Runtime: string;
-  Genre: string;
-  Director: string;
-  Actors: string;
-  Plot: string;
-  Language: string;
-  Country: string;
-  Awards: string;
-  Ratings: [];
-  Metascore: string;
-  imdbRating: string;
-  imdbVotes: string;
-  DVD: string;
-  BoxOffice: string;
-  Website: string;
-  Writer: string;
-  Production: string;
-  Response: string;
-}
-
-interface State {
-  loading: boolean;
-  movies: Movie[];
-  errorMessages: string | null;
-}
-
-enum ActionName {
-  REQUEST = 'SEARCH_MOVIES_REQUEST',
-  SUCCESS = 'SEARCH_MOVIES_SUCCESS',
-  FAILURE = 'SEARCH_MOVIES_FAILURE',
-}
-
-interface REQUEST {
-  type: ActionName.REQUEST;
-}
-interface SUCCESS {
-  type: ActionName.SUCCESS;
-  payload: Movie[];
-}
-
-interface FAILURE {
-  type: ActionName.FAILURE;
-  error: string;
-}
-
-type MovieActions = REQUEST | SUCCESS | FAILURE;
-
-const MOVIE_API_URL = 'http://www.omdbapi.com/?s=man&apikey=a3744c28';
-
-const initialState: State = {
-  loading: true,
-  movies: [],
-  errorMessages: null,
-};
-
-const reducer: React.Reducer<State, MovieActions> = (state, action): State => {
-  switch (action.type) {
-    case ActionName.REQUEST:
-      return {
-        ...state,
-        loading: true,
-        errorMessages: null,
-      };
-    case ActionName.SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        movies: action.payload,
-      };
-    case ActionName.FAILURE:
-      return {
-        ...state,
-        loading: false,
-        errorMessages: action.error,
-      };
-    default:
-      throw new Error();
-  }
-};
+import style from './dndSort.module.css';
 
 const App: React.FC = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const imageList: string[] = [
+    'https://images.pexels.com/photos/1830252/pexels-photo-1830252.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/2759658/pexels-photo-2759658.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/277253/pexels-photo-277253.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/1743165/pexels-photo-1743165.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/3246665/pexels-photo-3246665.png?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/1690351/pexels-photo-1690351.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/2086361/pexels-photo-2086361.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/2088203/pexels-photo-2088203.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://images.pexels.com/photos/5603660/pexels-photo-5603660.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  ];
 
-  useEffect(() => {
-    axios
-      .get(MOVIE_API_URL)
-      .then((res) => {
-        dispatch({ type: ActionName.SUCCESS, payload: res.data.Search });
-      })
-      .catch((err) => {
-        dispatch({ type: ActionName.FAILURE, error: err });
-      });
-  }, []);
-
-  const search = (searchValue: string): void => {
-    const MOVIE_SEARCH_URL = `http://www.omdbapi.com/?s=${searchValue}&apikey=a3744c28`;
-    dispatch({
-      type: ActionName.REQUEST,
-    });
-    axios
-      .get(MOVIE_SEARCH_URL)
-      .then((res) => {
-        dispatch({ type: ActionName.SUCCESS, payload: res.data.Search });
-      })
-      .catch((err) => {
-        dispatch({ type: ActionName.FAILURE, error: err });
-      });
-  };
-
-  const { movies, errorMessages, loading } = state;
-
-  console.log('movies', movies, loading);
   return (
-    <>
-      <Header text="MovieSearch" />
-      <Container component="main" maxWidth="md">
-        <CssBaseline />
-        <Box mt={4} textAlign="center">
-          <Search search={search} />
-          <Typography component="p">
-            Sharing a few of our favorite movies
-          </Typography>
-        </Box>
-        <Box
-          mt={4}
-          display="flex"
-          flexWrap="wrap"
-          justifyContent="space-around">
-          {loading && !errorMessages ? (
-            <Typography component="p"> loading...</Typography>
-          ) : errorMessages ? (
-            <Typography component="p">{errorMessages}</Typography>
-          ) : (
-            movies.map((movie: Movie, index: number) => (
-              <MovieList key={`${index}-${movie.Title}`} movie={movie} />
-            ))
-          )}
-        </Box>
-      </Container>
-    </>
+    <div className={style.body}>
+      <div className={style.container}>
+        {imageList.map((item: string) => (
+          <div key={item} className={style.imageCard}>
+            <img src={item} alt="ソート可能な画像" className={style.image} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 export default App;
--- src/CalendarElement/components/calendarElement.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import dayjs from 'dayjs';
-import { Typography } from '@material-ui/core';
-
-export const CalendarElement: React.FC = () => {
-  const today = dayjs();
-  return <div>col test</div>;
-};
